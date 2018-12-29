3822ec56e8b43036d337904b452a3e7b3fbf6fbb
tc9011 <tc9011@126.com>
2018-12-29 15:40:44 +0800

    refactor(all): rename project

--- README.md
+++ README.md
@@ -4,11 +4,6 @@
 an component library for angular
 
 
-
-立个 flag，把公司 react 组件库出个 angular 版本
-
-
-
 ## TODO
 
 - [ ] 组件库代码初始化
--- components/radio/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of radio
- */
-
-export * from './lib/radio.service';
-export * from './lib/radio.component';
-export * from './lib/radio.module';
