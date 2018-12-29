8b5242cdb0910cee9474e5b610a55ac495df33a7
tc9011 <tc9011@126.com>
2018-12-30 00:43:52 +0800

    feat(components): add library settings

---
+++ components/button/button.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button.component';
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonModule { }
