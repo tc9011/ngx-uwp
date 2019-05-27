1cda552307d65d128b9d39590be7e350eabfe2bd
tc9011 <tc9011@126.com>
2019-05-27 15:29:12 +0800

    feat(navbar): add navbar

--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'homepage',
+        redirectTo: 'home',
         pathMatch: 'full'
       },
       {
-        path: 'homepage',
+        path: 'home',
         loadChildren: './homepage/homepage.module#HomepageModule',
       },
       {
