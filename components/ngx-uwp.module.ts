066b62439715a936701f0f664749079f11a6b2ee
tc9011 <tc9011@126.com>
2019-06-04 14:04:45 +0800

    feat: add tooltip settings

--- components/ngx-uwp.module.ts
+++ components/ngx-uwp.module.ts
@@ -3,16 +3,19 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { UwpButtonModule } from 'ngx-uwp/button';
 import { UwpIconModule } from 'ngx-uwp/icon';
 import { UwpThemeModule } from 'ngx-uwp/core';
+import { UwpTooltipModule } from 'ngx-uwp/tooltip';
 
 export * from 'ngx-uwp/button';
 export * from 'ngx-uwp/icon';
 export * from 'ngx-uwp/core';
+export * from 'ngx-uwp/tooltip';
 
 @NgModule({
   exports: [
     UwpButtonModule,
     UwpIconModule,
     UwpThemeModule,
+    UwpTooltipModule,
   ]
 })
 export class NgxUWPModule {
---
+++ components/tooltip/public_api.ts
@@ -0,0 +1,2 @@
+export * from './uwp-tooltip.module';
+export * from './uwp-tooltip.component';
